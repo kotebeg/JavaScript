95395fe013857f9b759b7a71b43f236e81e6d979
kotebeg <49250158+kotebeg@users.noreply.github.com>
2025-01-12 11:30:06 +0400

    Update main_funcitons.js
    adding js features

--- main_funcitons.js
+++ main_funcitons.js
@@ -5,3 +5,11 @@
 
 
 document.getElementById("demo").innerHTML = "Hello JavaScript!"
+
+document.getElementById('myImage').src='pic_bulbon.gif'
+document.getElementById('myImage').src='pic_bulboff.gif'
+
+document.getElementById("demo").style.fontSize = "35px";
+
+document.getElementById("demo").style.display = "none";
+document.getElementById("demo").style.display = "block";
