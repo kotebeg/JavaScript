ff91659b8c342693cfbbdc0ebdd4f7f70eaf00c9
kotebeg <49250158+kotebeg@users.noreply.github.com>
2025-01-15 22:42:46 +0400

    Update main_funcitons.js
    Adding and Deleting Elements

--- main_funcitons.js
+++ main_funcitons.js
@@ -27,7 +27,26 @@ console.log(5 + 6);
 window.print()  // print existing web page to printer
 
 // Finding HTML Elements
-
 document.getElementById(id)	          //Find an element by element id
 document.getElementsByTagName(name)	  // Find elements by tag name
 document.getElementsByClassName(name) //Find elements by class name
+
+
+// Changing HTML Elements
+element.innerHTML =  new html content	 // Change the inner HTML of an element
+element.attribute = new value          // 	Change the attribute value of an HTML element
+element.style.property = new style     // Change the style of an HTML element
+element.setAttribute(attribute, value) // Change the attribute value of an HTML element
+
+
+// Adding and Deleting Elements
+document.createElement(element)   // Create an HTML element
+document.removeChild(element)     // Remove an HTML element
+document.appendChild(element)     // Add an HTML element
+document.replaceChild(new, old)   // Replace an HTML element
+document.write(text)              // Write into the HTML output stream
+
+
+
+
+
