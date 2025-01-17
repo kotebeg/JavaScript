d1ac14baa8077200ea06690a50db96624c074318
kotebeg <49250158+kotebeg@users.noreply.github.com>
2025-01-17 23:44:09 +0400

    Update main_funcitons.js
    this descriptions

--- main_funcitons.js
+++ main_funcitons.js
@@ -26,6 +26,16 @@ const person = {
   }
 };
 
+/*
+In an object method, this refers to the object.
+Alone, this refers to the global object.
+In a function, this refers to the global object.
+In a function, in strict mode, this is undefined.
+In an event, this refers to the element that received the event.
+Methods like call(), apply(), and bind() can refer this to any object.
+
+*/
+
 
 
 // Display Possibilities
